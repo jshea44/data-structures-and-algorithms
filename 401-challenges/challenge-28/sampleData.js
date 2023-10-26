3537fdd5b4d6bf71e298d56d983b7eac77d63d46
joshuashea <joshshea44@gmail.com>
2023-10-25 18:07:19 -0700

    inital for challenge 28

---
+++ 401-challenges/challenge-28/sampleData.js
@@ -0,0 +1,29 @@
+const movies = [
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    genres: ["Drama"],
+  },
+  {
+    title: "Pulp Fiction",
+    year: 1994,
+    genres: ["Crime", "Drama"],
+  },
+  {
+    title: "The Godfather",
+    year: 1972,
+    genres: ["Crime", "Drama"],
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    genres: ["Action", "Adventure", "Sci-Fi"],
+  },
+  {
+    title: "Forrest Gump",
+    year: 1994,
+    genres: ["Drama", "Romance"],
+  },
+];
+
+module.exports = movies;
